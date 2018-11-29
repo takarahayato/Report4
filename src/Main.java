f4265657c4436dd3654748e102292b86b3514b9d
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2018-11-29 15:10:02 +0900

    first commit second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullpointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
